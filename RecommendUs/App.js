5b3fb2b748aff9fa6db30cc2f7c6effe3f219e24
Danielfib <dfb2@cin.ufpe.br>
2018-05-03 01:17:09 -0300

    incluindo react-navigate e navegando para tela de grupo

--- RecommendUs/App.js
+++ RecommendUs/App.js
@@ -1,23 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, View, Text } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+//home (atualmente vazia)
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen abaiudbwe</Text>
+        
+        {/*botao pra trocar de tela*/}
+        <Button
+          title="criar grupo!"
+          onPress={() => this.props.navigation.navigate('CriarGrupo')}
+        />        
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+//tela de criar grupo
+class CreateGroup extends React.Component {
+  render() {
+    return (
+      <View style ={{flex: 1, alignItems:'center', justifyContent: 'center'}}>
+        <Text>
+          Tela de criar grupo
+        </Text>
+      </View>
+    );
+  }
+}
+
+const RootStack = StackNavigator({
+  //essa é´a pilha de telas, a ordem de apresentacao aqui importa
+  //ou seje, a tela que vier primeiro aqui será o index
+  Home: {
+    screen: HomeScreen,
   },
+  CriarGrupo: {
+    screen: CreateGroup,
+  },  
+  
+  initialRouteName: 'Home',  
 });
+
+//export defaul class é o "main"
+export default class App extends React.Component {
+  render(){
+    return <RootStack />;
+  }
+}
