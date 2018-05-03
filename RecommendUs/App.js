a4884b1cdbbdae64aea83872dd8ef3cf6219bf2a
Danielfib <dfb2@cin.ufpe.br>
2018-05-03 01:57:38 -0300

    separando a tela de grupo para outro .js

--- RecommendUs/App.js
+++ RecommendUs/App.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { default as CreateGroup } from './screens/createGroup';
 
 //home (atualmente vazia)
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen abaiudbwe</Text>
+        <Text>Home Screeniii</Text>
         
         {/*botao pra trocar de tela*/}
         <Button
@@ -19,21 +20,8 @@ class HomeScreen extends React.Component {
   }
 }
 
-//tela de criar grupo
-class CreateGroup extends React.Component {
-  render() {
-    return (
-      <View style ={{flex: 1, alignItems:'center', justifyContent: 'center'}}>
-        <Text>
-          Tela de criar grupo
-        </Text>
-      </View>
-    );
-  }
-}
-
 const RootStack = StackNavigator({
-  //essa é´a pilha de telas, a ordem de apresentacao aqui importa
+  //essa é a pilha de telas, a ordem de apresentacao aqui importa
   //ou seje, a tela que vier primeiro aqui será o index
   Home: {
     screen: HomeScreen,
