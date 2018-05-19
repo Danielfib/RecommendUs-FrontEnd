d1708942b8c5a1f3f2e71b7362aa8c0d832d14f7
rpss2 <rpss@cin.ufpe.br>
2018-05-19 18:29:21 -0300

    Starting preferences second screen

--- RecommendUs/src/navigation/home.js
+++ RecommendUs/src/navigation/home.js
@@ -2,8 +2,14 @@ import {
     StackNavigator,
 } from 'react-navigation'
 
+import Preference2 from 'screens/app/home/preference2'
+
 const HomeNavigator = StackNavigator(
-    {},
+    {
+        prefereces2: {
+            screen: Preference2
+        },
+    },
     {
         headerMode: 'none',
         navigationOptions: {
---
+++ RecommendUs/src/screens/app/home/preference2.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+class Preference2 extends React.Component {}
+
+export default Preference2
