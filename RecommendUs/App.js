b6adc25f560f6e37fe5bd837661bb672eaefe5fb
Danielfib <dfb2@cin.ufpe.br>
2018-05-21 18:13:41 -0300

    [PLOTTWIST] removing navigation and changing drgabble implementation

--- RecommendUs/App.js
+++ RecommendUs/App.js
@@ -1,43 +1,72 @@
 import React, { Component } from 'react';
-import { Button, View, Text } from 'react-native';
+import { Button, 
+         View, 
+         Text, 
+         Animated, 
+         StyleSheet, 
+         PanResponder } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import CreateGroup from './screens/createGroup';
 
-const NAV_BAR_H = 20;
+export default class daggable extends Component {
+  constructor(){
+    super();
 
-//home (atualmente vazia)
-class HomeScreen extends React.Component {
-  render() {
+    this.state = {
+      pan: new Animated.ValueXY()
+    };
+  }
+
+  //called when react renders a component (before render method)
+  componentWillMount(){
+    //listener for the delta value change
+    this._val = {x:0, y:0}
+    this.state.pan.addListener((value) => this._val = value);
+
+    //initialize panResponder with move handling
+    this.PanResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (e, gesture) => true,
+      onPanResponderMove: Animated.event([
+        null, { dx: this.state.pan.x, dy: this.state.pan.y}
+      ]),
+      //animates the return when user releases the circle
+      onPanResponderRelease: (e, gesture) => {
+        Animated.spring(this.state.pan, {
+          toValue: {x:0, y:0},
+          friction: 5
+        }).start();
+      }
+      //adjusting delta value
+      //this.state.pan.setValue({x:0, y:0})
+    });
+  }
+
+  render(){
+    const panStyle = {
+      transform: this.state.pan.getTranslateTransform()
+    }
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        
-        {/*botao pra trocar de tela*/}
-        <Button
-          title="criar grupo!"
-          onPress={() => this.props.navigation.navigate('CriarGrupo')}
-        />        
-      </View>
+      <Animated.View
+        {...this.PanResponder.panHandlers}
+        style={[panStyle, styles.circle]}
+      >
+        <Text style = {styles.dragabble_Text}>Oi!</Text>
+      </Animated.View>
     );
   }
 }
 
-const RootStack = StackNavigator({
-  //essa é a pilha de telas, a ordem de apresentacao aqui importa
-  //ou seje, a tela que vier primeiro aqui será o index
-  Home: {
-    screen: HomeScreen,
+let CIRCLE_RADIUS = 30;
+let styles = StyleSheet.create({
+  circle: {
+    backgroundColor: "skyblue",
+    width: CIRCLE_RADIUS * 2,
+    height: CIRCLE_RADIUS * 2,
+    borderRadius: CIRCLE_RADIUS
   },
-  CriarGrupo: {
-    screen: CreateGroup,
-  },  
-  
-  initialRouteName: 'Home',  
-});
-
-//export defaul class é o "main"
-export default class App extends React.Component {
-  render(){
-    return <RootStack />;
+  dragabble_Text: {
+    textAlign : 'center',
+    marginTop : 25,
+    color     : '#f;ff'
   }
-}
+});
