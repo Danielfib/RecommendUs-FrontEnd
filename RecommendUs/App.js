a13054b58edb317ee4f54b893634527bc2376c77
Danielfib <dfb2@cin.ufpe.br>
2018-05-22 00:50:11 -0300

    multiple draggable circles (but no dinamially created)

--- RecommendUs/App.js
+++ RecommendUs/App.js
@@ -1,72 +1,127 @@
-import React, { Component } from 'react';
-import { Button, 
-         View, 
-         Text, 
-         Animated, 
-         StyleSheet, 
-         PanResponder } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-//import CreateGroup from './screens/createGroup';
+import React, { Component } from "react";
+import { StyleSheet, View, Text, PanResponder, Animated,  } from "react-native";
 
-export default class daggable extends Component {
-  constructor(){
-    super();
+class Draggable extends Component {
+  constructor(props) {
+    super(props);
 
     this.state = {
-      pan: new Animated.ValueXY()
+      showDraggable: true,
+      dropAreaValues: null,
+      pan: new Animated.ValueXY(),
+      opacity: new Animated.Value(1)
     };
   }
 
-  //called when react renders a component (before render method)
-  componentWillMount(){
-    //listener for the delta value change.
-    this._val = {x:0, y:0}
+  componentWillMount() {
+    this._val = { x:0, y:0 }
     this.state.pan.addListener((value) => this._val = value);
 
-    //initialize panResponder with move handling
-    this.PanResponder = PanResponder.create({
-      onStartShouldSetPanResponder: (e, gesture) => true,
-      onPanResponderMove: Animated.event([
-        null, { dx: this.state.pan.x, dy: this.state.pan.y}
-      ]),
-      //animates the return when user releases the circle
-      onPanResponderRelease: (e, gesture) => {
-        Animated.spring(this.state.pan, {
-          toValue: {x:0, y:0},
-          friction: 5
-        }).start();
-      }
-      //adjusting delta value
-      //this.state.pan.setValue({x:0, y:0})
-    });
+    this.panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gesture) => true,
+        onPanResponderGrant: (e, gesture) => {
+          this.state.pan.setOffset({
+            x: this._val.x,
+            y:this._val.y
+          })
+          this.state.pan.setValue({ x:0, y:0})
+        },
+        onPanResponderMove: Animated.event([ 
+          null, { dx: this.state.pan.x, dy: this.state.pan.y }
+        ]),
+        onPanResponderRelease: (e, gesture) => {
+          if (this.isDropArea(gesture)) {
+            Animated.timing(this.state.opacity, {
+              toValue: 0,
+              duration: 1000
+            }).start(() =>
+              this.setState({
+                showDraggable: false
+              })
+            );
+          } 
+        }
+      });
   }
 
-  render(){
+  isDropArea(gesture) {
+    return gesture.moveY < 200;
+  }
+
+  render() {
+    return (
+      <View style={{ width: "20%", alignItems: "center" }}>
+        {this.renderDraggable()}
+      </View>
+    );
+  }
+
+  renderDraggable() {
     const panStyle = {
       transform: this.state.pan.getTranslateTransform()
     }
+    if (this.state.showDraggable) {
+      return (
+        <View style={{ position: "absolute" }}>
+          <Animated.View
+            {...this.panResponder.panHandlers}
+            style={[panStyle, styles.circle, {opacity:this.state.opacity}]}
+          />
+        </View>
+      );
+    }
+  }
+}
+
+
+export default class App extends Component {
+  render() {
     return (
-      <Animated.View
-        {...this.PanResponder.panHandlers}
-        style={[panStyle, styles.circle]}
-      >
-        <Text style = {styles.dragabble_Text}>Oi!</Text>
-      </Animated.View>
+      <View style={styles.mainContainer}>
+        <View style={styles.dropZone}>
+          <Text style={styles.text}>Drop them here!</Text>
+        </View>
+        <View style={styles.ballContainer} />
+        <View style={styles.row}>
+          <Draggable />
+          <Draggable />
+          <Draggable />
+          <Draggable />
+          <Draggable />
+        </View>
+      </View>
     );
   }
 }
 
 let CIRCLE_RADIUS = 30;
-let styles = StyleSheet.create({
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1
+  },
+  ballContainer: {
+    height:200
+  },
   circle: {
     backgroundColor: "skyblue",
     width: CIRCLE_RADIUS * 2,
     height: CIRCLE_RADIUS * 2,
     borderRadius: CIRCLE_RADIUS
   },
-  dragabble_Text: {
-    textAlign : 'center',
-    marginTop : 25,
-    color     : '#fff'
+  row: {
+    flexDirection: "row"
+  },  
+  dropZone: {
+    height: 200,
+    backgroundColor: "#00334d"
+  },
+  text: {
+    marginTop: 25,
+    marginLeft: 5,
+    marginRight: 5,
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 25,
+    fontWeight: "bold"
   }
 });
