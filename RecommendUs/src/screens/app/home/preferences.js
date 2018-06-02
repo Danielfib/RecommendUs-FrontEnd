dcb98d6b36a21b1c3437d057a87fe62e803596d5
rpss2 <rpss@cin.ufpe.br>
2018-06-02 13:03:35 -0300

    style details

--- RecommendUs/src/screens/app/home/preferences.js
+++ RecommendUs/src/screens/app/home/preferences.js
@@ -1,34 +1,55 @@
 import React from 'react';
-import  {  View, Text, StyleSheet } from 'react-native';
+import  {  View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
 import Calendar from '../../../components/calendar'
 import MultisliderButton from '../../../components/multislider'
 import PickerButton from '../../../components/pickerButton'
+
+import BarStatus from '../../../components/StatusBar'
+import Header from '../../../components/Header'
+import CardChoose from '../../../components/CardChoose'
+import ImageCircle from '../../../components/ImageCircle'
 import NextButton from '../../../components/NextButton'
+
+import em from '../../../properties/responsive'
 
 export default class Preferences extends React.Component { 
     render() {
       return (
         <View style = {style.mainContainer}>
+          <BarStatus/>
+          <Header>
+              <Text style={style.titleHeader}>
+                  {"Preferências"}
+              </Text>
+          </Header>
           <View style = {style.container}>
-          
-          </View>
-          <View style = {[style.container, {height:'20%'}]}>
-            <Text style = {style.textTitle}>Quanto você quer pagar?</Text>
-            <View style = {[style.containerSlider, style.container]}>  
-            <MultisliderButton/>
+            <View style={style.friendsView}>
+                <View style={style.friendsPhotos}></View>
+                <Text style={style.friendsText}>
+                    {"Esperando Confirmação"}
+                </Text>
             </View>
-          </View>
-          <View style = {style.container}>
-            <Text style = {style.textTitle}>Quando vai ser?</Text>
-            <Calendar/>
-          </View>
+            <View style = {[style.container, {marginTop: em (5)}]}>
+              <Text style = {style.textTitle}>Quanto você quer pagar?</Text>
+              <View style = {[style.containerSlider, style.container]}>  
+                <MultisliderButton/>
+              </View>
+            </View>
+            <View style = {style.container}>
+              <Text style = {style.textTitle}>Quando vai ser?</Text>
+              <Calendar/>
+            </View>
 
-          <View style = {style.container}>
-            <Text style = {style.textTitle}>Onde vai ser?</Text>
-            <PickerButton/>
-            <View style = {style.buttomArrowRight}>
-              <NextButton/>
+            <View style = {[style.container, {marginTop: em (15)}]}>
+              <Text style = {style.textTitle}>Onde vai ser?</Text>
+              <PickerButton/>
+              <TouchableOpacity
+                style={style.nextButton}
+                onPress={() => this.props.navigation.navigate('prefereces2')}
+              >
+                  <NextButton />
+              </TouchableOpacity>
             </View>
           </View>
         </View>
@@ -38,25 +59,42 @@ export default class Preferences extends React.Component {
 
 const style = StyleSheet.create({
   mainContainer: {
-    height: '100%', 
-    backgroundColor:'#F5F5F5', 
+    flex: 1, 
+  },
+  titleHeader: {
+    color: '#FFFFFF',
+    fontWeight: '500',
+    fontSize: em (7.6),
   },
   container: {
-    height: '25%',
+    flex: 1,
+  },
+  friendsView: {
+    alignItems: 'center',
+    marginTop: em (3),
+  },
+  friendsPhotos: {
+      flexDirection: 'row',
+      marginBottom: em (3),
+  },
+  friendsText: {
+      color: '#A30000',
+      fontSize: em (4),
+      fontWeight: '500',
   },
   textTitle: {
-    fontSize: 28,
-    color: '#000',
-    marginLeft: '8%',
+    fontSize: em(8),
+    color: '#000000',
+    marginLeft: em (8),
     fontFamily: 'Roboto',
-    fontWeight: '400' 
+    fontWeight: '500' 
   },
   buttomArrowRight: {
-    flex:1,
+    flex: 1,
     alignItems: 'flex-end',
     justifyContent: 'flex-end',
-    marginRight: '6%',
-    paddingBottom: '3%'
+    marginRight: em (6),
+    paddingBottom: em (5),
   }, 
   containerSlider: {
     flex: 1,
@@ -66,6 +104,11 @@ const style = StyleSheet.create({
     marginTop: 0,
     marginBottom: 0,
     padding: 0,
+  },
+  nextButton: {
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
   },
 })
 
