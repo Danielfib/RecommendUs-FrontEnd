84afd2e658a2ad92149dd957422c8e31829e47f3
rpss2 <rpss@cin.ufpe.br>
2018-07-08 03:44:02 -0300

    missing detail

--- RecommendUs/src/screens/app/login/login.js
+++ RecommendUs/src/screens/app/login/login.js
@@ -127,7 +127,7 @@ const styles = StyleSheet.create({
         height: em (178),
     },
     subContainer: {
-        backgroundColor: 'transparent',
+        position: 'absolute',
         alignSelf: 'center',
     },
     logo: {
